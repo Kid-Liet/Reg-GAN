7e18496516aca000b49d23339047c7a3a25102fa
Kid Liet <52225942+Kid-Liet@users.noreply.github.com>
2021-11-05 18:14:19 +0800

    Update UnitTrainer.py

--- trainer/UnitTrainer.py
+++ trainer/UnitTrainer.py
@@ -43,7 +43,7 @@ class Unit_Trainer(nn.Module):
             betas=(0.5, 0.999),
         )
         if config["regist"]:
-            self.R_A = Reg().cuda()
+            self.R_A = Reg(config['size'], config['size']).cuda()
             self.spatial_transform = Transformer_2D().cuda()
             self.optimizer_R_A = torch.optim.Adam(
                 self.R_A.parameters(), lr=config["lr"], betas=(0.5, 0.999)
