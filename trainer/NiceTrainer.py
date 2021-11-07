fc0e5a992f397fae0ecc1e1b3d3ec2ccc7adc17e
Kid Liet <52225942+Kid-Liet@users.noreply.github.com>
2021-11-07 17:26:43 +0800

    Update NiceTrainer.py

--- trainer/NiceTrainer.py
+++ trainer/NiceTrainer.py
@@ -25,7 +25,7 @@ class Nice_Trainer():
         ## def networks
         
         if config['regist']:
-            self.R_A = Reg(config['size'], config['size']).cuda()
+            self.R_A = Reg(config['size'], config['size'],config['input_nc'],config['input_nc']).cuda()
             self.spatial_transform = Transformer_2D().cuda()
             self.optimizer_R_A = torch.optim.Adam(self.R_A.parameters(), lr=config['lr'], betas=(0.5, 0.999))
         if config['bidirect']:
