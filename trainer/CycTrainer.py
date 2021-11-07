0df46529fa52a62c5d702fe2474aa21518ff1c7a
Kid Liet <52225942+Kid-Liet@users.noreply.github.com>
2021-11-07 17:23:35 +0800

    Update CycTrainer.py

--- trainer/CycTrainer.py
+++ trainer/CycTrainer.py
@@ -30,7 +30,7 @@ class Cyc_Trainer():
         self.optimizer_D_B = torch.optim.Adam(self.netD_B.parameters(), lr=config['lr'], betas=(0.5, 0.999))
         
         if config['regist']:
-            self.R_A = Reg(config['size'], config['size']).cuda()
+            self.R_A = Reg(config['size'], config['size'],config['input_nc'],config['input_nc']).cuda()
             self.spatial_transform = Transformer_2D().cuda()
             self.optimizer_R_A = torch.optim.Adam(self.R_A.parameters(), lr=config['lr'], betas=(0.5, 0.999))
         if config['bidirect']:
