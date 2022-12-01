4ce0cd79a300cce8ba131ed9c1901d38bb1495a1
Kid Liet <52225942+Kid-Liet@users.noreply.github.com>
2022-12-01 19:05:29 +0800

    Update datasets.py

--- trainer/datasets.py
+++ trainer/datasets.py
@@ -5,26 +5,39 @@ import numpy as np
 from torch.utils.data import Dataset
 from PIL import Image
 import torchvision.transforms as transforms
+import torch
+
+
+
+
 
 class ImageDataset(Dataset):
-    def __init__(self, root,count = None,transforms_1=None,transforms_2=None, unaligned=False):
+    def __init__(self, root,noise_level,count = None,transforms_1=None,transforms_2=None, unaligned=False):
         self.transform1 = transforms.Compose(transforms_1)
         self.transform2 = transforms.Compose(transforms_2)
         self.files_A = sorted(glob.glob("%s/A/*" % root))
         self.files_B = sorted(glob.glob("%s/B/*" % root))
         self.unaligned = unaligned
+        self.noise_level =noise_level
         
     def __getitem__(self, index):
-        seed = np.random.randint(2147483647) # make a seed with numpy generator 
-        random.seed(seed) # apply this seed to img tranfsorms
-        
-        item_A = self.transform1(np.load(self.files_A[index % len(self.files_A)]).astype(np.float32))
-        #random.seed(seed)
-        if self.unaligned:
-            item_B = self.transform2(np.load(self.files_B[random.randint(0, len(self.files_B) - 1)]))
-        
-        else:  
+        if self.noise_level == 0:
+            # if noise =0, A and B make same transform
+            seed = np.random.randint(2147483647) # make a seed with numpy generator 
+            torch.manual_seed(seed)
+            torch.cuda.manual_seed(seed)
+            item_A = self.transform2(np.load(self.files_A[index % len(self.files_A)]).astype(np.float32))
+
+            torch.manual_seed(seed)
+            torch.cuda.manual_seed(seed)
             item_B = self.transform2(np.load(self.files_B[index % len(self.files_B)]).astype(np.float32))
+        else:
+            # if noise !=0, A and B make different transform
+            item_A = self.transform1(np.load(self.files_A[index % len(self.files_A)]).astype(np.float32))
+            item_B = self.transform1(np.load(self.files_B[index % len(self.files_B)]).astype(np.float32))
+            
+            
+            
         return {'A': item_A, 'B': item_B}
     def __len__(self):
         return max(len(self.files_A), len(self.files_B))
